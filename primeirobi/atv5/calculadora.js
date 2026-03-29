5e292361654dc052940455d3b9010ab0d753ba29
Mateus Mulinetti <mmulinete010@gmail.com>
2026-03-29 19:10:49 -0300

    Adicionando Atv5

---
+++ primeirobi/atv5/calculadora.js
@@ -0,0 +1,9 @@
+const valorConta = 200;
+const percentualGorjeta = 15;
+
+const valorGorjeta = valorConta * (percentualGorjeta / 100);
+const valorTotal = valorConta + valorGorjeta;
+   
+console.log("Valor da Conta: R$:", valorConta, "Gorjeta (15%):", valorGorjeta, "Total a pagar:", valorTotal);
+   
+
